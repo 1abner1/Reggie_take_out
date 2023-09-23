8ef6776512ef6296fda4cf067c68ba3b8ae0f78c
abner <axiaowenwen@163.com>
2023-09-23 19:47:22 +0800

    第二次提交

--- src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.itheima.reggie.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.common.CustomException;
 import com.itheima.reggie.entity.Category;
 import com.itheima.reggie.entity.Dish;
 import com.itheima.reggie.entity.Setmeal;
@@ -28,13 +29,13 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> i
 
         //添加查询条件，根据分类id 进行查询
         dishLambdaQueryWrapper.eq(Dish::getCategoryId,id);
-        int count1 = dishService.count();
-//        int count = dishService.count(dishLambdaQueryWrapper);
+        //int count1 = dishService.count();
+        int count1 = dishService.count(dishLambdaQueryWrapper);
 
         //查询当前分类是否关联了菜品，如果已经关联，抛出一个业务异常
         if(count1>0){
             //已经关联菜品，抛出一个业务异常
-            throw new ClassCastException("当前分类下关联了菜品，不能删除");
+            throw new CustomException("当前分类下关联了菜品，不能删除");
         }
 
         // 查询当前分类是否关联了套餐，如果已经失联，抛出一个业务异常
@@ -44,7 +45,7 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> i
         int count2 = setmealService.count();
         if(count2>0){
             //已经关联套餐，抛出一个业务异常
-            throw new ClassCastException("当前分类下关联了套餐，不能删除");
+            throw new CustomException("当前分类下关联了套餐，不能删除");
 
         }
 
